228c023662d81654b79a5df4ac6e508fa1f626e6
Asad Wazir <shafi444411@gmail.com>
2025-12-19 21:05:12 +0100

    Removed unnecessary file

--- area.py
+++
@@ -1,8 +0,0 @@
-number=0
-number =input("please enter a number to check wether it is even or odd")
-number=int(number)
-if   number%2 == 0:
-    print("the number is even")
-
-else:
-        print('the given number is odd')
--- dic.py
+++
@@ -1,3 +0,0 @@
-asad ={"asad":1,'shafi':2,'muskan':3}
-asad
-
